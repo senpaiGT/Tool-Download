1e7620e076914e558bce13f5dcb6cfc8f1d18961
senpaiGT <85949224+senpaiGT@users.noreply.github.com>
2021-06-15 17:44:16 +0300

    Add files via upload

---
+++ Tool-Download.sh
@@ -0,0 +1,24 @@
+echo " "
+echo " "
+echo " "
+echo "
+╱╭╮╱╱╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╭╮
+╭╯╰╮╱╱╱╱╱┃┃╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱┃┃
+╰╮╭╋━━┳━━┫┃╱╭━╯┣━━┳╮╭╮╭┳━╮┃┃╭━━┳━━┳━╯┃
+╱┃┃┃╭╮┃╭╮┃┃╱┃╭╮┃╭╮┃╰╯╰╯┃╭╮┫┃┃╭╮┃╭╮┃╭╮┃
+╱┃╰┫╰╯┃╰╯┃╰╮┃╰╯┃╰╯┣╮╭╮╭┫┃┃┃╰┫╭╮┃╰╯┃╰╯┃
+╱╰━┻━━┻━━┻━╯╰━━┻━━╯╰╯╰╯╰╯╰┻━┻╯╰┻━━┻━━╯BETA VERSİON "|lolcat
+echo " "
+echo " 
+[1] Ddoser Tool "|lolcat
+echo " 
+<─────[ What Do Use ]─────> "|lolcat
+echo -en "\e[93m #: \e[m"
+read answer
+echo "
+<───[ Okey Installing Please Wait. ]───> "|lolcat
+cd
+sleep 3
+git clone https://github.com/cyweb/hammer
+echo "
+<──[ Done Installing Do cd And See Tool ]──> "|lolcat 
